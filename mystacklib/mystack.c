2e90c82245c5d26cad58d822069170149b70a9ae
LeBrent <kofenoen@outlook.com>
2018-12-19 15:04:00 +0100

    optimized code for unit testing

--- mystacklib/mystack.c
+++ mystacklib/mystack.c
@@ -1,14 +1,15 @@
 #include "mystack.h"
 #include "logging.h"
 #include <stdlib.h>
+#include <string.h>
 /* The stack is an abstract data type.
- * this means that the internal structures are
- * never exposed to the users of the library.
- */
+* this means that the internal structures are
+* never exposed to the users of the library.
+*/
 
 /* Note: the stacks have no knowledge of what types
- * they are storing. This is not a concern of the stack
- */
+* they are storing. This is not a concern of the stack
+*/
 
 typedef struct stackObject* pStackObject_t;
 typedef struct stackObject
@@ -28,100 +29,96 @@ typedef struct stackMEta
 } StackMeta_t;
 
 static pStackMeta_t gStackList = NULL;
-pStackMeta_t lastElement = NULL;
+int handle = 1;
 
- int mystack_create(size_t objsize)
- {
- 	/* Hint: use gStackList */
+int mystack_create(size_t objsize)
+{
+	/* Hint: use gStackList */
 
 	if (objsize <= 0)
-	 {
+	{
 		return -1;
 	}
+
 	pStackMeta_t newstack = malloc(sizeof(StackMeta_t));
 
 	if(gStackList == NULL)
 	{
-	newstack->handle = 1;
-	newstack->objsize = objsize;
-	gStackList->next = newstack;
-	lastElement = newstack;
-  }
-	else if (lastElement->next == NULL)
-	{
-		newstack->handle = lastElement->handle + 1;
+		newstack->handle = handle;
 		newstack->objsize = objsize;
-		lastElement->next = newstack;
-		lastElement = newstack;
-	}
- 	DBG_PRINTF("Created stack with handle: %d and objsize %zu bytes\n", lastElement->handle, objsize);
- 	return 0;
- }
-
- int mystack_push(int handle, void* obj)
- {
-	 if(handle <= 0 || gStackList == NULL)
-	 {
-		 return -1;
-	 }
-	pStackMeta_t tempstack = gStackList;
-	pStackObject_t newObject = obj;
-
-	while (tempstack->handle != handle)
-	 {
-		tempstack = tempstack->next;
-	}
-	//Found the stack, lets adjust the objsize
-	tempstack->objsize += sizeof(obj);
-	//If there is nothing in the stack, easy! put the head direcly on the newobject.
-	if (tempstack->stack == NULL)
-	{
-		tempstack->stack = newObject;
+		gStackList = newstack;
 	}
 	else
 	{
-		//put the current head in the stack in a temp variable, then put the new object as the head of the stack, and put the temp variable as the stack->next
-		pStackObject_t tempObj = tempstack->stack;
-		tempstack->stack = newObject;
-		newObject->next = tempObj;
+		newstack->handle = handle;
+		newstack->objsize = objsize;
+		newstack->next = gStackList;
+		gStackList = newstack;
+		handle++;
 	}
+	DBG_PRINTF("Created stack with handle: %d and objsize %zu bytes\n", gStackList->handle, objsize);
+	return gStackList->handle;
+}
+
+int mystack_push(int handle, void* obj)
+{
+	if(handle <= 0 || gStackList == NULL || obj == NULL)
+	{
+		return -1;
+	}
+	pStackMeta_t tempstack = gStackList;
+	pStackObject_t newObject = (pStackObject_t)malloc(sizeof(StackObject_t));
+
+
+	while (tempstack->handle != handle)
+	{
+		tempstack = tempstack->next;
+	}
+	newObject->obj = malloc(sizeof(tempstack->objsize)); //malloc sizeof van obj
+	newObject->obj = obj;
+
+	//put the current head in the stack in a temp variable, then put the new object as the head of the stack, and put the temp variable as the stack->next
+	pStackObject_t tempObj = tempstack->stack;
+	tempstack->stack = newObject;
+	newObject->next = tempObj;
+
+	//newobject->obj = malloc(tempstack->objsize)
+	//tempstack->stack = tempobj;
 
 	tempstack->numelem++;
-  DBG_PRINTF("handle: %d\n, obj: %p\n", handle, obj);
- 	return 0;
- }
+	DBG_PRINTF("handle: %d\n, obj: %p\n", handle, obj);
+	return 0;
+}
 
-
-
- int mystack_pop(int handle, void* obj)
- {
-	 if (handle <=0 || gStackList == NULL)
-	 {
- 		return -2;
+int mystack_pop(int handle, void* obj)
+{
+	if (handle <=0 || gStackList == NULL || obj == NULL)
+	{
+		return -1;
 	}
 
- 	pStackMeta_t temp = gStackList;
+	pStackMeta_t temp = gStackList;
 
- 	while (temp!=NULL) {
- 		if (temp->handle == handle) {
- 			pStackObject_t toDestroy = temp->stack;
- 			temp->stack = temp->stack->next;
+	while (temp!=NULL) {
+		if (temp->handle == handle) {
+			pStackObject_t toDestroy = temp->stack;
 
- 			obj = toDestroy->obj;
- 			free(toDestroy);
+			memcpy(obj, toDestroy->obj, temp->objsize);
+			temp->stack = temp->stack->next;
+			//free(toDestroy->obj);
+			free(toDestroy);
 			temp->numelem--;
+			DBG_PRINTF("handle: %d, obj: %p\n", handle, obj);
+			return 0;
+		}
+		temp = temp->next;
+	}
+	return -1;
+}
 
- 			DBG_PRINTF("handle: %d, obj: %p\n", handle, obj);
- 			return 0;
- 		}
- 		temp = temp->next;
- 	}
- 	return -1;
- }
-
- int mystack_destroy(int handle)
- {
-    DBG_PRINTF("handle: %d\n", handle);
+int mystack_destroy(int handle)
+{
+	DBG_PRINTF("handle: %d\n", handle);
 
 	if(handle <= 0 || gStackList == NULL)
 	{
@@ -133,41 +130,39 @@ pStackMeta_t lastElement = NULL;
 	while (temp!= NULL)
 	{
 		if (temp->handle == handle)
-			{
-				pStackObject_t toDestroy = temp->stack;
+		{
+			pStackObject_t toDestroy = temp->stack;
 
-				//clearing the stackobject objects
-				while (toDestroy != NULL)
-				{
-					temp->stack = temp->stack->next;
-					free(toDestroy);
-				}
-				//destroy the stackmeta object
-				free(temp);
+			//clearing the stackobject objects
+			while (toDestroy != NULL)
+			{
+				temp->stack = temp->stack->next;
+				free(toDestroy);
 			}
-			temp = temp->next;
+			//destroy the stackmeta object
+			free(temp);
+		}
+		temp = temp->next;
 	}
 
- 	return 0;
- }
+	return 0;
+}
 
- int mystack_nofelem(int handle)
- {
-
-	 if (handle <= 0)
-	 {
-	 	return -1;
-	 }
-	 pStackMeta_t temp = gStackList;
-
-	 while (temp!= NULL)
-	 {
-		 if (temp->handle == handle)
-		 {
-			 return temp->numelem;
-		 }
-		 temp = temp->next;
-	 }
-    DBG_PRINTF("handle: %d\n", handle);
- 	return 0;
- }
+int mystack_nofelem(int handle)
+{
+	if (handle <= 0)
+	{
+		return -1;
+	}
+	pStackMeta_t temp = gStackList;
+	while (temp!= NULL)
+	{
+		if (temp->handle == handle)
+		{
+			return temp->numelem;
+		}
+		temp = temp->next;
+	}
+	DBG_PRINTF("handle: %d\n", handle);
+	return 0;
+}
--- mystacklib/stackTest.c
+++ mystacklib/stackTest.c
@@ -26,30 +26,46 @@ void test_destroy_previous_stack(void)
   TEST_ASSERT_EQUAL(0,mystack_destroy(1));
 }
 
-void test_NAME(void)
+void test_create_multiple_stacks(void)
 {
-  int test = 1337;
-
-  int handle = mystack_create(20);
-
-  TEST_ASSERT_EQUAL(0,mystack_push(handle, &test));
-
-  int res = -1;
-
-  TEST_ASSERT_EQUAL(0,mystack_pop(handle, &res));
-
-  // printf("res=%d\n", res);
-
-	TEST_ASSERT_EQUAL(test, res);
+  TEST_ASSERT_EQUAL(1,mystack_create(sizeof(int)));
+  TEST_ASSERT_EQUAL(2,mystack_create(sizeof(unsigned int)));
+  TEST_ASSERT_EQUAL(3,mystack_create(sizeof(char)));
+  TEST_ASSERT_EQUAL(4,mystack_create(34));
+  TEST_ASSERT_EQUAL(5,mystack_create(89));
 }
 
+void test_succesful_push(void)
+{
+
+  int number = 4;
+  unsigned int j = 20;
+  char c = 'B';
+  TEST_ASSERT_EQUAL(0,mystack_push(1,&number));
+  TEST_ASSERT_EQUAL(0,mystack_push(2,&j));
+  TEST_ASSERT_EQUAL(0,mystack_push(3,&c));
+}
+
+void test_succesful_pop(void)
+{
+  mystack_create(4);
+  mystack_create(4);
+  mystack_create(4);
+  int number = 34958;
+  int number2 = 0;
+  mystack_push(2,&number);
+  mystack_pop(2,&number2);
+  TEST_ASSERT_EQUAL_INT(number, number2);
+}
 int main() {
   UnityBegin("stackTest.c");
 
   MY_RUN_TEST(test_create_a_stack_and_get_the_handle);
   MY_RUN_TEST(test_destroy_previous_stack);
 
-	MY_RUN_TEST(test_NAME);
+	MY_RUN_TEST(test_create_multiple_stacks);
+  MY_RUN_TEST(test_succesful_push);
+  MY_RUN_TEST(test_succesful_pop);
 
   return UnityEnd();
 }
